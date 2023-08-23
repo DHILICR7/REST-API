5fd8434badab233fc7ea42b4666c8979663dcfdd
DHILICR7 <108514280+DHILICR7@users.noreply.github.com>
2023-08-23 09:31:06 +0530

    Add files via upload

---
+++ DAY__1/src/main/java/com/dhilii/sample/controller/Welcome.java
@@ -0,0 +1,12 @@
+package com.dhilii.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome";
+	}
+}
---
+++ DAY__1/src/test/java/com/dhilii/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhilii.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
